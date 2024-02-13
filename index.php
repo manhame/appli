905576a09a029e59132601a8f5433014f6375a24
manhame <respectbiz@gmail.com>
2024-02-13 10:32:53 +0100

    fonctions message et modifs panier, session

--- index.php
+++ index.php
@@ -3,34 +3,77 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <title>Ajout produit</title>
 </head>
 <body>
+    <?php 
+    session_start();
+     $totalQtt = 0;
+     foreach($_SESSION['products'] as $index=>$product) {
+         $totalQtt+=$product['qtt'];
+     }   
+    ?>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark d-flex justify-content-between pe-3 py-3">
+<ul class="navbar-nav ">
+          <li class="nav-item"><a class="nav-link" href="index.php">index.php</a></li>
+          <li class="nav-item"><a class="nav-link" href="recap.php">recap.php</a></li>
+    </ul>
+          <a href="recap.php"><button type="button" class="btn btn-primary position-relative">
+              Panier
+              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                  <?= $totalQtt ?>
+                </button></a>
+            </nav>        
+
+
     <h1>Ajouter un produit</h1>
     <form action="traitement.php" method="post">
         <p>
-            <label>
+            <label for="exampleInputText1"class="form_label">
                 Nom du produit :
-                <input type="text" name="name">
-            </label>
+                </label>   
+                <input type="text" class="form-control" name="name">
+                
         </p>
         <p>
-            <label>
+            <label for="exampleInputNumber1"class="form_label">
                 Prix du produit :
-                <input type="number" step="any" name="price">
-            </label>
+                </label>
+                <input type="number" class="form-control" step="any" name="price">
+            
         </p>
         <p>
-            <label>
+            <label for="exampleInputNumber1"class="form_label">
                 Quantité de produits :
-                <input type="number" name="qtt" value="1">
-            </label>
+                </label>    
+                <input type="number" class="form-control" name="qtt" value="1">
+           
         </p>
         <p>
-            <input type="submit" name="submit" value="Ajouter un produit">
+            <input type="submit" class="btn btn-primary" name="submit" value="Ajouter un produit">
+            
         </p>
+              
+<?php
+
+if (isset($_SESSION['message'])){
+    echo $_SESSION['message'];
+}/*
+if(!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+    echo "<div class='alert alert-warning' role='alert'>
+        aucun produit en session 
+        </div>";
+}
+    else {
+        echo "<div class='alert alert-warning' role='alert'>
+        Le produit a bien été ajouté au panier ! 
+        </div>";    
+    }
+     */  
+?>        
     </form>
 
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- traitement.php
+++ traitement.php
@@ -17,7 +17,26 @@
             ];
             $_SESSION['products'][]=$product; /*on enregistre la var $product, un tableau, dans la session, 
             un autre tableau contenant des "products" au pluriel*/
+            $_SESSION['message'] = "Le produit a bien été ajouté";
         }
     }
 
-    header("location:index.php"); /*si les conditions ne sont pas remplies -false, null, 0- redirection sur le fichier d'origine*/
+    header("location:index.php");
+    $_SESSION['message'] = "Echec, veuillez réessayer";
+    
+     /*si les conditions ne sont pas remplies -false, null, 0- redirection sur le fichier d'origine*/
+
+     if(isset ($_GET[action])){
+
+        switch($_GET[action]){
+
+            case "add" : <form action="traitement.php?action=add" method="post">
+            case "delete" : <form action="traitement.php?action=delete" method="post">,
+            case "clear" : <form action="traitement.php?action=clear" method="post">,
+            case "up-qtt" : <form action="traitement.php?action=up-qtt" method="post">,
+            case "down-qtt" : <form action="traitement.php?action=down-qtt" method="post">,
+        }
+        
+     }
+
+   
